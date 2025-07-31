a56027570920d5a16be837840d4203be8e2011bb
Alex  Njugi  Karanja <alexnjugi11@gmail.com>
2025-07-31 16:22:18 +0300

    Update user_routes.py

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,38 +1,39 @@
 from flask import Blueprint, request
 from app.services.user_service import *
 from app.schemas.user_schema import UserSchema
-from app.utils.jwt_handler import token_required
-from app.utils.role_required import admin_required
 
 user_bp = Blueprint('users', __name__)
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
 @user_bp.get('/')
-@token_required
-@admin_required
 def get_users():
     users = get_all_users()
-    return users_schema.dump(users)
+    return users_schema.dump(users), 200
 
 @user_bp.get('/me')
-@token_required
-def get_my_profile(current_user):
-    return user_schema.dump(current_user)
+def get_my_profile():
+    
+    dummy_user = get_user_by_id(1)
+    if not dummy_user:
+        return {"message": "Dummy user not found"}, 404
+    return user_schema.dump(dummy_user), 200
 
 @user_bp.put('/me')
-@token_required
-def update_my_profile(current_user):
-    data = request.get_json()
-    updated = update_user(current_user, data)
-    return user_schema.dump(updated)
+def update_my_profile():
+    dummy_user = get_user_by_id(1)
+    if not dummy_user:
+        return {"message": "Dummy user not found"}, 404
 
+    data = request.get_json()
+    updated = update_user(dummy_user, data)
+    return user_schema.dump(updated), 200
+
+# Public: Assign role (e.g., promote/demote user)
 @user_bp.put('/<int:user_id>/role')
-@token_required
-@admin_required
 def assign_user_role(user_id):
     user = get_user_by_id(user_id)
     if not user:
         return {"message": "User not found"}, 404
     data = request.get_json()
-    return user_schema.dump(assign_role(user, data.get("role")))
+    return user_schema.dump(assign_role(user, data.get("role"))), 200
